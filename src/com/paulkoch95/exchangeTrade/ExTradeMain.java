b1cc36f812d20bbbf19d7a83caec07a21bd3272a
Paul Koch <paulkoch95@gmail.com>
2015-01-10 21:53:25 +0100

    messin arround...almost no progress

--- src/com/paulkoch95/exchangeTrade/ExTradeMain.java
+++ src/com/paulkoch95/exchangeTrade/ExTradeMain.java
@@ -3,11 +3,20 @@ package com.paulkoch95.exchangeTrade;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Properties;
+import java.util.UUID;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -16,14 +25,26 @@ import java.io.IOException;
 
 
 
-public class ExTradeMain extends JavaPlugin{
+public class ExTradeMain extends JavaPlugin implements Listener{
 	
 	public int allowedItem = 3;
+	public String PATH_TO_PROPDATA = System.getProperty("user.dir")+"/plugins/ex_trade_dependencies/storeProperties.properties";
+	public String CURRENCY_NAME = "Gulden";
+	private HashMap<UUID, Integer> money = new HashMap<>();
+	
 	public void onEnable(){
+		getServer().getPluginManager().registerEvents(this, this);
 		getLogger().info("Exchange Trade erfolgreich geladen!");
 		loadPropData();
 		
 		
+	}
+	public void onDisable(){
+		//savePropData();
+		for (Entry<UUID, Integer> entry : money.entrySet()) {
+			getConfig().set(entry.getKey() + "."+CURRENCY_NAME, entry.getValue());
+		}
+		saveConfig();
 	}
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		Player player = (Player) sender;//new instance of player is created
@@ -42,7 +63,7 @@ public class ExTradeMain extends JavaPlugin{
 				}
 				if(args[0].equalsIgnoreCase("debug")){
 					
-					player.sendMessage("Key Content quals: "+getKeyContent("player"));
+					player.sendMessage("Your Balance is: "+money.get(player.getUniqueId()));
 				}
 				if(args[0].equalsIgnoreCase("info")){
 					
@@ -75,8 +96,49 @@ public class ExTradeMain extends JavaPlugin{
 		return (Integer) null;
 	}
 	
+	/////////////////
+	//Event Handler//
+	/////////////////
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e) {
+		Player p = e.getPlayer();
+		if (!getConfig().contains(p.getUniqueId().toString())) {
+			getConfig().set(p.getUniqueId() + "."+CURRENCY_NAME, 0);
+			money.put(p.getUniqueId(), 0);
+			getLogger().info("NEUER SPIELER!");
+		} else {
+			money.put(p.getUniqueId(), getConfig().getInt(p.getUniqueId() + "."+CURRENCY_NAME));
+		}
+	}
+	
+	@EventHandler
+	public void onEntityDeath(EntityDeathEvent e) {
+		if (e.getEntity() instanceof Monster) {
+			if (e.getEntity().getKiller() instanceof Player) {
+				Player p = e.getEntity().getKiller();
+				//giveMoney(p, 200);
+				removeMoney(p,200);
+			}
+		} else if (e.getEntity() instanceof Villager) {
+			if (e.getEntity() instanceof Player) {
+				Player p = e.getEntity().getKiller();
+				removeMoney(p, 200);
+			}
+		}
+	}
 	
 	
+	
+	private void giveMoney(Player p, int amount) {
+		UUID uuid = p.getUniqueId();
+		money.put(uuid, money.get(uuid) + amount);
+		p.sendMessage("§2§l$" + amount + CURRENCY_NAME+" earned!");
+	}
+	private void removeMoney(Player p, int amount) {
+		UUID uuid = p.getUniqueId();
+		money.put(uuid, money.get(uuid) - amount);
+		p.sendMessage("§c§l$" + amount + CURRENCY_NAME+" lost!");
+	}
 	
 	//////////////////////
 	//Properties loading//
@@ -85,8 +147,8 @@ public class ExTradeMain extends JavaPlugin{
 	
 	public void loadPropData(){
 		try {
-			prop.load(new FileInputStream(System.getProperty("user.dir")+"/playerData.properties"));//load .properties Data by FIS
-			getLogger().info("Player Data succesfully loaded!");
+			prop.load(new FileInputStream(PATH_TO_PROPDATA));//load .properties Data by FIS
+			getLogger().info("Store Data succesfully loaded!");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -102,7 +164,7 @@ public class ExTradeMain extends JavaPlugin{
 	}
 	public void savePropData(){
 		try {
-			prop.store(new FileOutputStream("playerData.properties"), null);//save .properties Data
+			prop.store(new FileOutputStream(PATH_TO_PROPDATA), null);//save .properties Data
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
